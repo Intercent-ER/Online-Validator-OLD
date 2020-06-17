526e9473e3000f5102622bf47474fa85edbdb924
Luca-Regione <66590021+Luca-Regione@users.noreply.github.com>
2020-06-17 15:32:28 +0200

    Aggiornato il controllo per l'estensione del file

--- OnlineValidator/src/com/onlinevalidator/controller/ResultController.java
+++ OnlineValidator/src/com/onlinevalidator/controller/ResultController.java
@@ -4,12 +4,15 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.onlinevalidator.utils.FormatCheckerInterface;
 
 @Controller
 public class ResultController {
@@ -18,6 +21,10 @@ public class ResultController {
 	String fileContent;
 	public static final String MIME_TEXT_PLAIN = "text/plain";
 	String finalResult = "";
+	@Autowired
+	FormatCheckerInterface formatChecker;
+	boolean format;
+	
 	
 	@RequestMapping("/uploader")
 	public String fileUploader() {
@@ -28,8 +35,10 @@ public class ResultController {
 
 	@RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
 	public @ResponseBody String uploadFileHandler(@RequestParam("file") MultipartFile file) {
+		
+		format = formatChecker.checkFormat(file.getContentType());
 
-		if (!file.isEmpty() && file.getContentType().equals(MIME_TEXT_PLAIN)) {
+		if (!file.isEmpty() && format) {
 			try {
 				fileName = file.getOriginalFilename();
 				byte[] bytes = file.getBytes();
@@ -59,7 +68,7 @@ public class ResultController {
 			}
 		} else {
 
-			if(!file.getContentType().equals(MIME_TEXT_PLAIN)) {
+			if(!format) {
 				finalResult += "Il formato del file non è corretto, deve essere txt";
 				System.out.println(file.getContentType());
 			}
